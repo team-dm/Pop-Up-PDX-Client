dbd9d9cecab9e24aa62b381378141e46f5658b99
Dyanna Tolman <dyannast@gmail.com>
2019-06-27 09:47:39 -0700

    wireframe mock up renders on browser

--- pop-up-client/src/App.js
+++ pop-up-client/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+     <h2>Pop Up PDX</h2>
+    <div className="popUpList">
+      <ul>
+        <a href="#">
+        <li>
+          <h3>Pop Up #1</h3>
+          <img src="https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+        </li>
         </a>
-      </header>
+        <a href="#">
+        <li>
+          <h3>Pop Up #2</h3>
+          <img src="https://images.unsplash.com/photo-1501262139015-998ac3d1e0be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+        </li>
+        </a>
+        <a href="#">
+        <li>
+          <h3>Pop Up #3</h3>
+          <img src="https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+        </li>
+        </a>
+      </ul>
+    </div>
     </div>
   );
 }
